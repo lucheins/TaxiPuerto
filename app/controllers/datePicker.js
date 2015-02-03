ec99813ec424b6f23e063e113604a402f44b9965
Luis <calero.luis@gmail.com>
2015-02-02 23:01:51 -0500

    First Commit

---
+++ app/controllers/datePicker.js
@@ -0,0 +1,86 @@
+var args = arguments[0] || {};
+
+
+function reportDate(date) {
+	
+	var dateValue = $.pickDay.value;
+	var day = dateValue.getDate();
+    date.day = day.toString();
+    var month = dateValue.getMonth() + 1;
+    date.month = month.toString();
+    var year = dateValue.getFullYear();
+    date.year = year.toString();
+	
+    Ti.API.info('User selected: ' + day, month, year);
+    Ti.App.Properties.setString('day', date.day);
+    Ti.App.Properties.setString('month', date.month);
+    Ti.App.Properties.setString('year', date.year);
+}
+function getDate(date){
+	var day = Ti.App.Properties.getString('day');
+	var month = Ti.App.Properties.getString('month');
+	var year = Ti.App.Properties.getString('year');
+	
+	if (day || month || day){
+		$.pickDay.value = new Date(year,month - 1,day);
+	} else {
+	
+	var dateValue = $.pickDay.value;
+	var day = dateValue.getDate();
+    date.day = day.toString();
+    var month = dateValue.getMonth() + 1;
+    date.month = month.toString();
+    var year = dateValue.getFullYear();
+    date.year = year.toString();
+	
+    Ti.API.info('User selected: ' + day, month, year);
+    Ti.App.Properties.setString('day', date.day);
+    Ti.App.Properties.setString('month', date.month);
+    Ti.App.Properties.setString('year', date.year);
+   }
+}
+function reportTime() {
+	
+	var hourValue = $.pickHour.value;
+	var hour = hourValue.getHours();
+    hour = hour.toString();
+    var minute = hourValue.getMinutes();
+    minute = minute.toString();
+    
+    Ti.API.info('User selected: ' + hour, minute);    
+    Ti.App.Properties.setString('hour', hour);
+    Ti.App.Properties.setString('minute', minute);
+
+}
+function times() {
+		var hourValue = $.pickHour.value;
+		var hour = hourValue.getHours();
+	    hour = hour.toString();
+	    var minute = hourValue.getMinutes();
+	    minute = minute.toString();
+	    
+	    
+	    Ti.App.Properties.setString('hour', hour);
+	    Ti.App.Properties.setString('minute', minute);
+}
+
+function getTime(){
+	var hour = Ti.App.Properties.getString('hour');
+	var minute = Ti.App.Properties.getString('minute');
+	
+	if (hour || minute){
+	var value = new Date();
+	value.setMinutes(minute);
+	value.setHours(hour);
+	
+	$.pickHour.value = value;
+	
+	}
+}
+function datosDate(){
+	$.datePicker.close();
+}
+
+$.datePicker.open({modal: true});
+
+
---
+++ app/controllers/index.js
@@ -0,0 +1,192 @@
+function datePick(){
+	Alloy.createController("datePicker").getView().open();
+}
+function datos(e, when, pickup, airport, home){
+	var idClick = e.source.id || "";
+	switch (idClick) {
+		case "ready":
+		datos.when = idClick;
+		$.date.hide();
+		$.addresses.height = '65%';
+		$.scrollable.moveNext();
+		break;
+		
+		case "later":
+		datos.when = idClick;
+		$.scrollable.moveNext();
+		$.addresses.height = '55%';
+		break;
+		
+		case "home":
+		datos.pickup = idClick;
+		$.addClass($.homePick, 'first');
+		$.removeClass($.homePick, 'second');
+		$.addClass($.airportPick, 'second');
+		$.removeClass($.airportPick, 'first');
+		$.scrollable.moveNext();
+		break;
+		
+		case "airport":
+		datos.pickup = idClick;
+		$.addClass($.airportPick, 'first');
+		$.removeClass($.airportPick, 'second');
+		$.addClass($.homePick, 'second');
+		$.removeClass($.homePick, 'first');
+		$.scrollable.moveNext();
+		break;
+		
+		case "aircode1":
+		$.scrollable.moveNext();
+		break;
+		
+		case "homecode1":
+		$.scrollable.moveNext();
+		break;
+		
+		case "confirmar":
+		console.log(datos.when+ "," +datos.pickup);
+		break;
+	}
+	
+  // Alloy.createController("desde").getView().open();
+}
+$.index.open();
+function cleanDates() {
+	Ti.App.Properties.removeProperty('day');
+	Ti.App.Properties.removeProperty('month');
+	Ti.App.Properties.removeProperty('year');
+	Ti.App.Properties.removeProperty('hour');
+	Ti.App.Properties.removeProperty('minute');
+}
+function getDates(date){
+	date.day = Ti.App.Properties.getString('day');
+	date.month = Ti.App.Properties.getString('month');
+	date.year = Ti.App.Properties.getString('year');
+	date.hour = Ti.App.Properties.getString('hour');
+	date.minute = Ti.App.Properties.getString('minute');
+	Ti.API.info(date.day,date.month,date.year,date.hour,date.minute);
+}
+var lat, lng, altitud;
+var Map = require('ti.map');
+
+var mapview = Map.createView({
+	width:'100%',
+	top:0,
+	height: '100%',
+	mapType: Map.NORMAL_TYPE,
+	animate:true,
+	regionFit:true,
+	userLocation:true,
+	visible: true,
+	name: 'mapview',
+	keepScreenOn: true,
+});
+$.map.add(mapview);
+
+function NavRules(){
+Ti.Geolocation.headingFilter = 10;
+
+//ANDROID NAV
+if(Ti.Platform.osname  ==  'android') {
+
+   	Ti.Geolocation.Android.manualMode = true;
+   	// modify only the gps
+    var gpsProvider = Ti.Geolocation.Android.createLocationProvider({
+        name: Ti.Geolocation.PROVIDER_GPS,
+        minUpdateDistance: 0.0,
+    	minUpdateTime: 0
+    });
+    Ti.Geolocation.Android.addLocationProvider(gpsProvider);
+ 
+    //modify network
+   	var networkProvider = Ti.Geolocation.Android.createLocationProvider({
+	    name: Ti.Geolocation.PROVIDER_NETWORK,
+	    minUpdateTime: 3, 
+	    minUpdateDistance: 10
+	});
+	
+	Ti.Geolocation.Android.addLocationProvider(networkProvider);
+	
+	//Declare location rules
+	var gpsRule = Ti.Geolocation.Android.createLocationRule({
+	    // NO PROVIDER SPECIFIED IN ORDER TO MAKE IT A GENERAL RULE FOR ALL PROVIDERS
+	    provider: Ti.Geolocation.PROVIDER_GPS,
+	    // Updates should be accurate to 100m
+	    accuracy: 100,
+	    // Updates should be no older than 30 seconds
+	    maxAge: 5000,
+	    // But  no more frequent than once per 10 seconds
+	    minAge: 3000,
+	});
+	Ti.Geolocation.Android.addLocationRule(gpsRule);
+	
+	}
+	
+//IOS NAV
+else {
+    Ti.Geolocation.distanceFilter    =    10;
+    Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
+    Ti.Geolocation.accuracy    =    Ti.Geolocation.ACCURACY_BEST;
+	}
+}//END NavRules
+
+NavRules();
+
+function getLocation(){	   
+		Titanium.Geolocation.getCurrentPosition(function(e){
+			if (e.error || !e.success) {
+        		console.log('getcurrentposition error');
+        		return; 
+    		}
+    		if (e.success){
+    		location_coords = e.coords;
+    		var pin_inicio = Map.createAnnotation({
+				latitude: e.coords.latitude,
+				longitude: e.coords.longitude,
+				title: "Ubicacion Inicial",
+				pincolor: Map.ANNOTATION_YELLOW,
+				animate:true,
+				myid:1
+			});
+ 			mapview.addAnnotation(pin_inicio);
+    		}
+			var region={
+				latitude: e.coords.latitude,
+				longitude: e.coords.longitude,
+				animate:true,
+				latitudeDelta:0.002,
+				longitudeDelta:0.002
+			};
+			mapview.setRegion(region);
+			
+		});	
+}//END FUNCTION GEOLOCATION
+
+getLocation();
+
+
+
+mapview.addEventListener('regionchanged', function(evt) {
+
+    Titanium.Geolocation.reverseGeocoder(evt.latitude,evt.longitude,function(dir)
+        {
+            direccion = dir.places;
+            
+            Ti.API.info("reverse geolocation result = "+JSON.stringify(dir));
+ 			address = direccion[0].address.split(",");
+ 			if (address.length < 9){
+ 				address.unshift("-");
+ 			};
+ 			
+ 			street = direccion[0].street1;
+ 			local = address[0];
+ 			sector = address[2];
+ 			barrio = address[3];
+ 			ciudad = address[4];
+ 			
+ 			console.log(street, sector, barrio, ciudad, local);		
+        
+  		});	
+});
+
+// $.scrollable.scrollingEnabled = false;
